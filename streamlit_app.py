3c703546407c8249a2362976a0dd405fe9b1b9ce
ShivaniGit13 <107093982+ShivaniGit13@users.noreply.github.com>
2023-05-21 17:20:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title('my motivational firm')
+streamlit.title('my parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry otatmeal')
+streamlit.text('Kale Spinach and rocket smoothy')
+streamlit.text('hard-boiled free-range egg')
+
